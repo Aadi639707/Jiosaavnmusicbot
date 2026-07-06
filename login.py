d479a1a0212b82363a09cca16e9f2ac1b5cf7b0b
Aadi639707 <aadi639707@gmail.com>
2026-07-06 11:08:09 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,5 @@
-
+from pyrogram import Client
+api_id = 31283416 # Tumhari API ID
+api_hash = "b69f8f202f9dd4781ac4c79677a5ea9e"
+with Client("AnnonAssistant", api_id=api_id, api_hash=api_hash) as app:
+    print("Session generated successfully!")
