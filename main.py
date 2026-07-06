f3261b5f0ec05017c4c16578afb6a2101f0a7898
Aadi639707 <aadi639707@gmail.com>
2026-07-06 11:09:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+import asyncio
+from pyrogram import idle
+from core import app, assistant, call
+async def start_bot():
+    print("Starting Bot Client...")
+    await app.start()
+    print("Starting Assistant Client...")
+    await assistant.start()
+    print("Starting PyTgCalls (VC Client)...")
+    await call.start()
+    print("Bot is Alive and Lightning Fast! 🚀")
+    # Yeh line bot ko 24/7 zinda rakhegi
+    await idle()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_bot())
